c37f6035de89eceb1f85881ce26ffe844342efcc
AdibRasel <97701792+AdibRasel@users.noreply.github.com>
2022-11-02 08:20:12 +0600

    সূরা আল ফাতিহা
    সূরা আল ফাতিহা

---
+++ my_app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import Sura_All_Fatiha from './Sura/Sura_All_Fatiha/Sura_All_Fatiha';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Sura_All_Fatiha />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
